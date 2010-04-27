21593daa782a28ce91b65f42ae814102edbf03bc
xmxsuperstar <xmxsuperstar@2fff646b-a759-572a-912d-0d619c9727e5>
2010-04-27 18:27:55 +0000

    now can support ssl for multi hosts

--- trunk/ptplocal/src/main/java/ptp/local/SSLForwardServer.java
+++ trunk/ptplocal/src/main/java/ptp/local/SSLForwardServer.java
@@ -3,7 +3,6 @@ package ptp.local;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.net.SocketTimeoutException;
 import java.security.KeyStore;
 
 import javax.net.ssl.SSLContext;
@@ -17,53 +16,53 @@ import org.apache.log4j.Logger;
 import ptp.Config;
 import ptp.util.ByteArrayUtil;
 
-public class SSLForwardServer implements Runnable {
+public class SSLForwardServer {
 	private static Logger log = Logger.getLogger(SSLForwardServer.class);
 
-	private boolean isStopped = false;
+	private String destHost;
+	private int destPort;
 
-	public synchronized void stopServer() {
-		isStopped = true;
+	public SSLForwardServer(String destHost, int destPort) {
+		this.destHost = destHost;
+		this.destPort = destPort;
 	}
 
-	@Override
-	public void run() {
+	public int start() {
 
 		String ksName = "/etc/ptp.jks";
 		char ksPass[] = "phptunnelproxykspass".toCharArray();
 		char ctPass[] = "phptunnelproxyctpass".toCharArray();
+		int localSslPort = Integer.parseInt(Config.getIns().getValue(
+				"ptp.local.ssl.port", "8889"));
 		try {
 			KeyStore ks = KeyStore.getInstance("JKS");
 			ks.load(SSLForwardServer.class.getResourceAsStream(ksName), ksPass);
 			SSLContext sc = SSLContext.getInstance("SSLv3");
 			sc.init(new X509KeyManager[] { new AliasKeyManager(ks, ctPass,
-					"twitter.com") }, null, null);
+					this.destHost) }, null, null);
 			SSLServerSocketFactory ssf = sc.getServerSocketFactory();
-			int localSslPort = Integer.parseInt(Config.getIns().getValue(
-					"ptp.local.ssl.port", "8889"));
-			SSLServerSocket sslServerSocket = (SSLServerSocket) ssf
-					.createServerSocket(localSslPort);
-			sslServerSocket.setSoTimeout(1000);
-			log.info("local ssl server started on port: " + localSslPort);
-
-			while (!isStopped) {
-				SSLSocket sslSocket = null;
+			
+			SSLServerSocket sslServerSocket = null;
+			while (true) {
 				try {
-					sslSocket = (SSLSocket) sslServerSocket.accept();
-				} catch (SocketTimeoutException ste) {
-					continue;
-				}
-				if (sslSocket != null) {
-					Thread sslForwardServerProcessThread = new Thread(
-							new SSLForwardServerProcessThread(sslSocket));
-					sslForwardServerProcessThread.start();
+					sslServerSocket = (SSLServerSocket) ssf
+							.createServerSocket(localSslPort);
+					break;
+				} catch (java.net.BindException be) {
+					localSslPort++;
 				}
 			}
-			sslServerSocket.close();
-			log.info("stop ssl server");
+			sslServerSocket.setSoTimeout(10000);
+			log.info("local ssl server started on port: " + localSslPort);
+
+			Thread sslForwardServerProcessThread = new Thread(
+					new SSLForwardServerProcessThread(sslServerSocket, destHost, destPort));
+			sslForwardServerProcessThread.start();
+			
 		} catch (Exception e) {
 			log.error(e.getMessage(), e);
 		}
+		return localSslPort;
 	}
 }
 
@@ -71,15 +70,22 @@ class SSLForwardServerProcessThread implements Runnable {
 	private static Logger log = Logger
 			.getLogger(SSLForwardServerProcessThread.class);
 
-	SSLSocket sslSocket;
+	SSLServerSocket sslServerSocket;
+	private String destHost;
+	private int destPort;
 
-	SSLForwardServerProcessThread(SSLSocket sslSocket) {
-		this.sslSocket = sslSocket;
+	SSLForwardServerProcessThread(SSLServerSocket sslServerSocket, String destHost, int destPort) {
+		this.sslServerSocket = sslServerSocket;
+		this.destHost = destHost;
+		this.destPort = destPort;
 	}
 
 	@Override
 	public void run() {
 		try {
+
+			SSLSocket sslSocket = (SSLSocket) sslServerSocket.accept();
+
 			DataInputStream in = new DataInputStream(sslSocket.getInputStream());
 
 			DataOutputStream out = new DataOutputStream(sslSocket
@@ -100,11 +106,8 @@ class SSLForwardServerProcessThread implements Runnable {
 			byte[] requestBody = ByteArrayUtil.trim(buff, headerLen + 2,
 					readCount - headerLen - 2);
 
-			String destHost = null;
+			//String destHost = null;
 			for (String header : requestHeaders) {
-				if (header.toUpperCase().startsWith("HOST: ".toUpperCase())) {
-					destHost = header.split(":\\s")[1];
-				}
 
 				if (header.toUpperCase().startsWith(
 						"Content-Length:".toUpperCase())) {
@@ -120,13 +123,6 @@ class SSLForwardServerProcessThread implements Runnable {
 			}
 			log.info("destHost: " + destHost);
 			destHost = Config.getIns().getIp(destHost);
-
-			int destPort = 443;
-			if (Boolean.parseBoolean(Config.getIns().getValue(
-					"ptp.local.https.ashttp", "false"))) {
-				destPort = 80;
-				log.info("https used as http in remote");
-			}
 			log.info("destPort: " + destPort);
 
 			log.debug("ssl headers: " + requestHeaderString);
@@ -136,15 +132,10 @@ class SSLForwardServerProcessThread implements Runnable {
 			HttpProxy httpProxy = new HttpProxy(destHost, destPort, in, out);
 			httpProxy.proces(requestHeaders, requestBody);
 
-			try {
-				Thread.sleep(200);
-			} catch (InterruptedException e) {
-				log.fatal(e.getMessage(), e);
-			}
-
 			in.close();
 			out.close();
 			sslSocket.close();
+			sslServerSocket.close();
 			log.info("ssl forward end!");
 		} catch (IOException e) {
 			log.error(e.getMessage(), e);
--- trunk/ptplocal/src/main/java/ptp/ui/CLILauncher.java
+++ trunk/ptplocal/src/main/java/ptp/ui/CLILauncher.java
@@ -1,13 +1,12 @@
 package ptp.ui;
 
 import ptp.local.LocalProxyServer;
-import ptp.local.SSLForwardServer;
 import ptp.pac.PacServer;
 
 public class CLILauncher extends Launcher {
 	public static void main(String[] args) {
-		Thread sslForwarderThread = new Thread(new SSLForwardServer());
-		sslForwarderThread.start();
+		/*Thread sslForwarderThread = new Thread(new SSLForwardServer());
+		sslForwarderThread.start();*/
 		Thread localProxyThread = new Thread(new LocalProxyServer());
 		localProxyThread.start();
 		Thread pacThread = new Thread(new PacServer());
