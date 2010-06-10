e5cd17f21af4d6ef650e97b02bdcaba1f1f33c2c
xmxsuperstar <xmxsuperstar@2fff646b-a759-572a-912d-0d619c9727e5>
2010-06-10 12:33:11 +0000

    add new implementation win lauch script

--- branches/v1/ptplocal/src/test/java/ptp/net/test/LocalProxyServerTry.java
+++ branches/v1/ptplocal/src/test/java/ptp/net/test/LocalProxyServerTry.java
@@ -1,17 +1,21 @@
 package ptp.net.test;
 
 import ptp.net.LocalProxyServer;
+import ptp.pac.PacServer;
 import ptp.ui.Launcher;
 
-public class LocalProxyServerTry extends Launcher{
+public class LocalProxyServerTry extends Launcher {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		LocalProxyServer lps = new LocalProxyServer();
 		try {
+			PacServer pacServer = new PacServer();
+			Thread pacThread = new Thread(pacServer);
+			pacThread.start();
+			LocalProxyServer lps = new LocalProxyServer();
 			lps.startService();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
